2f69e956da71d4ddad0fd2c07291e0834e2718c8
Josué Urcia <129638387+Jos1616@users.noreply.github.com>
2023-07-08 01:02:25 -0500

    Archivos trabajados en restart inserción

---
+++ while-loop.py
@@ -0,0 +1,23 @@
+print("Welcome to Guess the Number!")
+
+print("The rules are simple. I will think of a number, and you will try to guess it.")
+
+import random
+
+number = random.randint(1,10)
+
+isGuessRight = False
+
+while isGuessRight != True:
+    #Ask the user for a guess.
+    guess = input("Guess a number between 1 and 10: ")
+    if int(guess) == number: #Is the guess the correct number?
+        print("You guessed {}. That is correct! You win!".format(guess))
+        #If the correct guess, tell the user it was the correct guess and exit the loop.
+        isGuessRight = True
+    else:
+        # If the user has not guessed the correct answer, enter the loop.
+        print("You guessed {}. Sorry, that isn’t it. Try again.".format(guess))
+        #If the wrong guess, tell the user it was the wrong guess and continue the loop.
+        
+#Notese que el valor de isGuessRight es el responsable del bucle
